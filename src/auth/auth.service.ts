9dd06d8de3c411268a6564d5b99f44e4ca52f382
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2024-10-20 11:55:59 +0000

    saving progress

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { compareSync } from 'bcrypt'
+import { Injectable, UnauthorizedException, NotAcceptableException } from '@nestjs/common';
+import { compareSync, genSaltSync, hashSync } from 'bcrypt'
 import { JwtService } from '@nestjs/jwt';
 
 import { UsersService } from '../users/users.service';
+import { bcryptConstants } from './constants';
 
 @Injectable()
 export class AuthService {
@@ -14,16 +15,37 @@ export class AuthService {
         if (user == null || compareSync(password, user.hasedPassword)) {
             throw new UnauthorizedException()
         }
-        const payload  = { id: user.id }
-        return {
-            access_token: await this.jwtService.signAsync(payload),
-            user: {
-                id: user.id,
+        const payload  = { 
+            id: user.id,
                 email: user.email,
                 firstName: user.firstName,
                 lastName: user.lastName,
                 createdAt: user.createdAt
-            }
+         }
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+            user: payload
+        }
+    }
+
+    async signup(email: string, firstName: string, lastName: string, password: string) {
+         const user = await this.usersService.user({ email })
+         if (user) {
+            throw new NotAcceptableException()
+         }
+         const newUser = await  this.usersService.createUser({
+            email, firstName, lastName, hasedPassword: hashSync(password, genSaltSync(bcryptConstants.saltRounds))
+         })
+         const payload  = { 
+            id: newUser.id,
+                email: newUser.email,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                createdAt: newUser.createdAt
+         }
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+            user: payload
         }
     }
 }
