67ed0d003edb6da294176810b7bd0e23998b942f
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2024-10-20 16:42:00 +0000

    saving progress

--- src/role/role.decorator.ts
+++ src/role/role.decorator.ts
@@ -1,3 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
+import { Role } from 'prisma/prisma-client'
 
-export const Role = (...args: string[]) => SetMetadata('role', args);
+
+export const ROLES_KEY = 'roles';
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
